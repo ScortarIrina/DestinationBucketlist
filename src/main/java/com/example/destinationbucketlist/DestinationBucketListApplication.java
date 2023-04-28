7fc5e1812a345b1747b5980a3a5feef6f4e6dbf9
Irina Scortar <irinascortar@gmail.com>
2023-04-28 17:06:59 +0300

    first commit

---
+++ src/main/java/com/example/destinationbucketlist/DestinationBucketListApplication.java
@@ -0,0 +1,13 @@
+package com.example.destinationbucketlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DestinationBucketListApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DestinationBucketListApplication.class, args);
+    }
+
+}
